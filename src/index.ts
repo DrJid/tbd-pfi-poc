b545487b1106eb8ff9e1562ee66b1c096347f803
Maijid Moujaled <maijid@chippercash.com>
2024-01-26 17:21:24 -0800

    Change /

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello TBD!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at on ${port}`);
 });
+
+
+// https://drjid-tbd-pfi-poc-2e4cb9365d06.herokuapp.com/
