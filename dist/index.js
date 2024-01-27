c9631725d68d04153d658a1fab4d5bfa9bc06667
Maijid Moujaled <maijid@chippercash.com>
2024-01-26 18:05:16 -0800

    Adding tbdex basic code

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,95 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+import { TbdexHttpServer, } from '@tbdex/http-server';
+const port = process.env.PORT || 5001;
+class CustomOfferingsApiProvider {
+    async getOffering(opts) {
+        console.log("getOffering called with", opts);
+        /*
+        const [ result ] =  await dataProvider.queryForOffering('offering', opts.id);
+    
+        if (!result) {
+          return undefined
+        }
+    
+        return Offering.factory(result.offering)
+        */
+        return undefined;
+    }
+    async getOfferings() {
+        console.log("getOfferings called");
+        /*
+        const results =  await dataProvider.getOfferings('offering');
+    
+        const offerings: Offering[] = []
+        for (let result of results) {
+          const offering = Offering.factory(result.offering)
+          offerings.push(offering)
+        }
+    
+        return offerings
+        */
+        return undefined;
+    }
+}
+class CustomExchangeApiProvider {
+    async write({ message }) {
+        console.log('write', message);
+    }
+    async getExchanges(opts) {
+        console.log('getExchanges called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getExchange(opts) {
+        console.log('getExchange called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getRfq(opts) {
+        console.log('getRfq called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getQuote(opts) {
+        console.log('getQuote called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getOrder(opts) {
+        console.log('getOrder called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getOrderStatuses(opts) {
+        console.log('getOrderStatuses called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+    async getClose(opts) {
+        console.log('getClose called with', opts);
+        // Implement your logic here and return the appropriate value
+        return undefined;
+    }
+}
+const exchangesApiImpl = new CustomExchangeApiProvider();
+const offeringsApiImpl = new CustomOfferingsApiProvider();
+const httpApi = new TbdexHttpServer({
+    exchangesApi: exchangesApiImpl,
+    offeringsApi: offeringsApiImpl
 });
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+httpApi.api.get('/', (req, res) => {
+    res.send('Please use the tbdex protocol to communicate with this server or a suitable library: https://github.com/TBD54566975/tbdex-protocol');
 });
+httpApi.submit('rfq', async (ctx, rfq) => {
+    await exchangesApiImpl.write({ message: rfq });
+});
+httpApi.submit('order', async (ctx, order) => {
+    await exchangesApiImpl.write({ message: order });
+});
+httpApi.submit('close', async (ctx, close) => {
+    await exchangesApiImpl.write({ message: close });
+});
+console.log(`Will open Mock PFI listening on port ${port}`);
+httpApi.listen(port, () => {
+    console.log(`Mock PFI listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,131 @@
 import express from 'express';
+import { 
+  TbdexHttpServer,
+  ExchangesApi, 
+  OfferingsApi, 
+  Offering,
+  Rfq, 
+  Order, 
+  Close, 
+  MessageKindClass, 
+  Quote, 
+  GetExchangesFilter,
+  OrderStatus,
+} from '@tbdex/http-server'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello TBD!');
-});
+class CustomOfferingsApiProvider implements OfferingsApi {
+  async getOffering(opts: {id: string}): Promise<Offering | undefined> {
+    console.log("getOffering called with", opts)
+    /*
+    const [ result ] =  await dataProvider.queryForOffering('offering', opts.id);
 
-app.listen(port, () => {
-  console.log(`Server running at on ${port}`);
-});
+    if (!result) {
+      return undefined
+    }
+
+    return Offering.factory(result.offering)
+    */
+    return undefined
+  }
+
+  async getOfferings(): Promise<Offering[] | undefined> {
+    console.log("getOfferings called")
+    /*
+    const results =  await dataProvider.getOfferings('offering');
+
+    const offerings: Offering[] = []
+    for (let result of results) {
+      const offering = Offering.factory(result.offering)
+      offerings.push(offering)
+    }
+
+    return offerings
+    */
+    return undefined
+  }
+}
+
+class CustomExchangeApiProvider implements ExchangesApi {
+
+  async write({ message }: { message: Rfq | Order | Close }) {
+    console.log('write', message);
+  }
+ 
+  async getExchanges(opts?: { filter: GetExchangesFilter }): Promise<MessageKindClass[][] | undefined> {
+    console.log('getExchanges called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+  }
+
+  async getExchange(opts: { id: string }): Promise<MessageKindClass[] | undefined> {
+    console.log('getExchange called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+  }
+
+  async getRfq(opts: { exchangeId: string }): Promise<Rfq | undefined> {
+    console.log('getRfq called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+  }
+
+  async getQuote(opts: { exchangeId: string }): Promise<Quote | undefined> {
+    console.log('getQuote called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+
+  }
+
+  async getOrder(opts: { exchangeId: string }): Promise<Order | undefined> {
+    console.log('getOrder called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+
+  }
+
+  async getOrderStatuses(opts: { exchangeId: string }): Promise<OrderStatus[] | undefined> {
+    console.log('getOrderStatuses called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+
+  }
+
+  async getClose(opts: { exchangeId: string }): Promise<Close | undefined> {
+    console.log('getClose called with', opts);
+    // Implement your logic here and return the appropriate value
+    return undefined
+
+  }
+}
+
+const exchangesApiImpl = new CustomExchangeApiProvider()
+const offeringsApiImpl = new CustomOfferingsApiProvider()
+
+const httpApi = new TbdexHttpServer({ 
+  exchangesApi: exchangesApiImpl, 
+  offeringsApi: offeringsApiImpl 
+})
+
+httpApi.api.get('/', (req, res) => {
+  res.send('Please use the tbdex protocol to communicate with this server or a suitable library: https://github.com/TBD54566975/tbdex-protocol')
+})
+
+httpApi.submit('rfq', async (ctx: any, rfq: Rfq) => {
+  await exchangesApiImpl.write({ message: rfq })
+})
+
+httpApi.submit('order', async (ctx: any, order: Order) => {
+  await exchangesApiImpl.write({ message: order })
+})
+
+httpApi.submit('close', async (ctx: any, close: Close) => {
+  await exchangesApiImpl.write({ message: close })
+})
+
+httpApi.listen(port, () => {
+  console.log(`Mock PFI listening on port ${port}`)
+})
 
 
-// https://drjid-tbd-pfi-poc-2e4cb9365d06.herokuapp.com/
